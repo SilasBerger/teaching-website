16a779f7511d77186c59bfadb4c8c3570dd3deb7
Silas Berger <silas.berger@outlook.com>
2023-12-31 10:26:25 +0100

    Implement proper YT component.

--- src/components/YouTubeVideo/YouTubeVideo.tsx
+++ src/components/YouTubeVideo/YouTubeVideo.tsx
@@ -4,19 +4,21 @@ export interface YouTubeVideoProps {
   videoId: string,
   title?: string,
   width?: string
-  height?: string,
 }
 
-export const YouTubeVideo = ({videoId, title, width, height}: YouTubeVideoProps) => {
+export const YouTubeVideo = ({videoId, title, width}: YouTubeVideoProps) => {
   return (
-    <iframe
-      src={`https://www.youtube-nocookie.com/embed/${videoId}`}
-      title={title ?? 'YouTube video player'}
-      width={ width ?? "560" }
-      height={ height ?? "315" }
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen>
-    </iframe>
+    <div className={styles.videoContainer} style={{width: width ?? "100%"}}>
+      <iframe
+        src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+        title={title ?? 'YouTube video player'}
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen>
+      </iframe>
+    </div>
   );
+
 }
