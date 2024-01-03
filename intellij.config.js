e8993b9b7e5a64fba83a3c7274e55c970a199e3d
Silas Berger <silas.berger@outlook.com>
2024-01-03 11:25:44 +0100

    Add workaround for @site imports in MDX plugin.

---
+++ intellij.config.js
@@ -0,0 +1,10 @@
+/*
+ * This is a workaround for aliased paths in .mdx files. IntelliJ's MDX plugin does not seem to pick up on the `paths`
+ * segment in the tsconfig. However, the IDE can apparently see this particular file (regardless of its name) and
+ * recognize the `System` object...
+ */
+System.config({
+  "paths": {
+    "@site/*": "./*",
+  }
+});
