4a8e674bf11bfe5a9294cbb17673b060b36c6a7d
Silas Berger <silas.berger@outlook.com>
2024-01-05 14:33:44 +0100

    Add definition list.

---
+++ src/components/DefinitionList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export default function DefinitionList(props: Props) {
+  return (
+    <dl className={styles.definitionList}>
+      {props.children}
+    </dl>
+  );
+}
