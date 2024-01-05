f62c75164b14b2ec3357444561208e8e2ae1b721
Silas Berger <silas.berger@outlook.com>
2024-01-05 23:58:09 +0100

    Implement AST-modifying remark plugin for MDI shorthands.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,6 +6,7 @@ import {buildScripts} from "./src/builder/scripts-builder";
 import {SCRIPTS_ROOT} from "./config/builder-config";
 import * as osPath from "path";
 import { Logger } from './src/builder/util/logger';
+import remarkMdi from "./src/plugins/remark-mdi";
 
 const siteConfig = loadSiteConfig();
 Logger.instance.info(`🔧 Building site '${siteConfig.siteId}'`);
@@ -20,7 +21,10 @@ const docsConfigs = scriptRoots.map((scriptRoot, index) => {
       id: `${scriptRoot}`.replace('/', '_'),
       path: `${SCRIPTS_ROOT}${scriptRoot}`,
       routeBasePath: `${scriptRoot}`,
-      sidebarPath: `./config/sidebars/${siteConfig.siteId}.sidebars.ts`
+      sidebarPath: `./config/sidebars/${siteConfig.siteId}.sidebars.ts`,
+      remarkPlugins: [
+        remarkMdi,
+      ]
     }
   ];
 });
