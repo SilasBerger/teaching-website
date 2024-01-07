0da82028218f7a81bf0079fd947f8ffea185aef4
Silas Berger <silas.berger@outlook.com>
2024-01-07 13:21:41 +0100

    Factor out esm import handling.

--- src/plugins/remark-fenced-blocks.ts
+++ src/plugins/remark-fenced-blocks.ts
@@ -1,95 +1,13 @@
 import {Transformer} from "unified";
 import {VFile} from "vfile";
-import {Literal, Node, Parent} from "unist";
+import {Parent} from "unist";
 import {visit} from "unist-util-visit";
 import {Directives} from "mdast-util-directive";
-
-function ensureEsmNode(mdast: Parent): Literal {
-  /*
-  let esmNode: Literal;
-  visit(mdast, 'mdxjsEsm', (node: Literal) => {
-    esmNode = node;
-  });
-
-  if (!esmNode) {
-    esmNode = {
-      type: 'mdxjsEsm',
-      value: ""
-    }
-    mdast.children = [esmNode, ...mdast.children];
-  }
-
-  return esmNode;
-
-   */
-
-  const esmNode = {
-    type: 'mdxjsEsm',
-    value: ""
-  }
-  mdast.children = [esmNode, ...mdast.children];
-  return esmNode;
-}
-
-function ensureImports(mdast: Parent, requiredImportStrings: string[]) {
-  const esmNode = ensureEsmNode(mdast);
-  const imports = (esmNode.value as string)
-    .split(';')
-    .map(importString => importString.trim());
-
-  requiredImportStrings
-    .map(importString => importString.trim())
-    .filter(importString => !!importString)
-    .map(importString => importString.replace(';', ''))
-    .filter(importString => !imports.includes(importString))
-    .forEach(importString => imports.push(importString));
-
-  esmNode.value = imports
-    .map(importString => `${importString};`)
-    .join('\n');
-}
-
-function ensureAdmonitionsImport(mdast: Parent) {
-  // ensureImports(mdast, [`import Admonition from "@site/src/theme/Admonition"`]);
-  const esmNode = ensureEsmNode(mdast);
-  // esmNode.value = `import Admonition from "@site/src/theme/Admonition";`;
-  esmNode.data = {
-    estree: {
-      type: 'Program',
-      sourceType: 'module',
-      body: [
-        {
-          type: 'ImportDeclaration',
-          specifiers: [
-            {
-              type: 'ImportDefaultSpecifier', // TODO: vs. ImportSpecifier
-              imported: {type: 'Identifier', name: 'Admonition'},
-              local: {type: 'Identifier', name: 'Admonition'},
-            }
-          ],
-          source: {
-            type: "Literal",
-            value: "@site/src/theme/Admonition",
-            // raw: "@site/src/theme/Admonition"
-          }
-        }
-      ]
-    }
-  }
-}
+import {ensureEsmImports, ImportType} from "./util/mdast-esm-imports-util";
 
 /** @type {import('unified').Plugin<[], MdastRoot>} */
 export default function remarkFencedBlocks(): Transformer {
-  return (mdast: Parent, file: VFile) => {
-    if (file.data.contentTitle === 'Hero Image') {
-      console.log(mdast);
-      return;
-    }
-
-    if (file.data.contentTitle !== 'Admonitions') {
-      return;
-    }
-
+  return (mdast: Parent, _: VFile) => {
     let foundAdmonition = false;
     visit(mdast, 'containerDirective', (fenceRoot: Parent & Directives, _: number, parent: Parent) => {
       foundAdmonition = true;
@@ -110,12 +28,11 @@ export default function remarkFencedBlocks(): Transformer {
     });
 
     if (foundAdmonition) {
-      console.log('creating import');
-      ensureAdmonitionsImport(mdast);
-    } else {
-      console.log('Not creating import');
+      console.log(`Found admonitions, ensuring import.`);
+      ensureEsmImports(mdast, [{
+        sourcePackage: '@site/src/theme/Admonition',
+        specifiers: [{type: ImportType.DEFAULT, name: 'Admonition'}],
+      }]);
     }
-
-    console.log(mdast);
   };
 };
---
+++ src/plugins/util/mdast-esm-imports-util.ts
@@ -0,0 +1,140 @@
+import {Parent} from "unist";
+import {visit} from "unist-util-visit";
+
+export enum ImportType {
+  DEFAULT = 'ImportDefaultSpecifier',
+  NAMED = 'ImportSpecifier',
+}
+
+export interface EsmImportSpecifier {
+  name: string,
+  type: ImportType;
+}
+
+export interface EsmImport {
+  sourcePackage: string,
+  specifiers: EsmImportSpecifier[];
+}
+
+interface ImportSpecifierEntry {
+  type: ImportType;
+  imported: {
+    type: 'Identifier';
+    name: string;
+  };
+  local: {
+    type: 'Identifier';
+    name: string;
+  };
+}
+
+interface ImportSource {
+  type: 'Literal';
+  value: string,
+}
+
+interface ImportDeclarationEntry {
+  type: 'ImportDeclaration';
+  specifiers: ImportSpecifierEntry[];
+  source: ImportSource;
+}
+
+interface EsmImportsNode {
+  type: 'mdxjsEsm',
+  data: {
+    estree: {
+      type: 'Program',
+      sourceType: 'module',
+      body: ImportDeclarationEntry[],
+    }
+  }
+}
+
+export function ensureEsmImports(mdast: Parent, importDeclarations: EsmImport[]): void {
+  console.log(`Working to ensure esm import.`);
+  const esmImportsNode = ensureEsmImportsNode(mdast);
+  importDeclarations.forEach(esmImport => {
+    const declarationEntry = ensureImportDeclarationEntry(esmImportsNode.data.estree.body, esmImport);
+    ensureImportSpecifierEntries(declarationEntry, esmImport);
+  });
+  console.log(mdast);
+}
+
+export function ensureImportDeclarationEntry(declarationEntries: ImportDeclarationEntry[], esmImport: EsmImport): ImportDeclarationEntry {
+  console.log(declarationEntries);
+  const existingEntry = declarationEntries
+    .filter(entry => entry.source && entry.source.value)
+    .find(entry => entry.source.value === esmImport.sourcePackage);
+
+  if (existingEntry) {
+    return existingEntry;
+  }
+
+  const declarationEntry: ImportDeclarationEntry = {
+    type: 'ImportDeclaration',
+    specifiers: [],
+    source: {
+      type: 'Literal',
+      value: esmImport.sourcePackage,
+    },
+  };
+  declarationEntries.push(declarationEntry);
+  return declarationEntry;
+}
+
+function ensureImportSpecifierEntries(declarationEntry: ImportDeclarationEntry, esmImport: EsmImport): void {
+  esmImport.specifiers
+    .forEach(specifier => ensureImportSpecifierEntry(declarationEntry, specifier))
+}
+
+function ensureImportSpecifierEntry(declarationEntry: ImportDeclarationEntry, esmImportSpecifier: EsmImportSpecifier): void {
+  const existingSpecifier = declarationEntry.specifiers
+    .find(specifier => specifier.imported.name === esmImportSpecifier.name);
+
+  if (existingSpecifier) {
+    return;
+  }
+
+  declarationEntry.specifiers.push({
+    type: esmImportSpecifier.type,
+    imported: {
+      type: 'Identifier',
+      name: esmImportSpecifier.name
+    },
+    local: {
+      type: 'Identifier',
+      name: esmImportSpecifier.name
+    },
+  });
+}
+
+function ensureEsmImportsNode(mdast: Parent): EsmImportsNode {
+  let esmImportsNode;
+  visit(mdast, 'mdxjsEsm', (node: any) => {
+    if (!node.data?.estree) {
+      return;
+    }
+
+    const estree = node.data.estree;
+    if (estree.type === 'Program' && estree.sourceType === 'module') {
+      esmImportsNode = node;
+    }
+  });
+
+  if (esmImportsNode) {
+    return esmImportsNode;
+  }
+
+  esmImportsNode = {
+    type: 'mdxjsEsm',
+    data: {
+      estree: {
+        type: 'Program',
+        sourceType: 'module',
+        body: [],
+      }
+    }
+  };
+  mdast.children = [esmImportsNode, ...mdast.children];
+  return esmImportsNode;
+}
