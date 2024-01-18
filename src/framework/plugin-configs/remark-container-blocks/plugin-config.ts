d3b51c8218281982db44a87f5ce4938c2fa42ef8
Silas Berger <silas.berger@outlook.com>
2024-01-18 17:12:39 +0100

    #16 Implement MDX shorthand for TabItem (Tab).

--- src/framework/plugin-configs/remark-container-blocks/plugin-config.ts
+++ src/framework/plugin-configs/remark-container-blocks/plugin-config.ts
@@ -2,10 +2,12 @@ import {ContainerDirectivesConfig} from "../../plugins/remark-container-directiv
 import admonitions from "./admonitions";
 import Hero from "./Hero";
 import Caption from "./Caption";
+import tabs from "./tabs";
 
 export const remarkContainerDirectivesConfig: ContainerDirectivesConfig = {
   declarations: [
     ...admonitions,
+    ...tabs,
     Hero,
     Caption,
   ]
