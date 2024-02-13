27439fdccdc322459dd5f97f37f6238cd98f65d3
Silas Berger <silas.berger@outlook.com>
2024-02-13 09:58:35 +0100

    Add align functionality to figure.

--- src/app/components/Figure/index.tsx
+++ src/app/components/Figure/index.tsx
@@ -1,12 +1,21 @@
+import styles from "./styles.module.scss";
+import clsx from "clsx";
+
 export interface Props {
   src: any;
   alt?: string;
   width?: string;
+  align?: string;
 }
 
-const Figure = ({src, alt, width}: Props) => {
+const Figure = ({src, alt, width, align}: Props) => {
   return (
-    <img src={src} alt={alt ?? ''} width={width ?? '100%'}/>
+    <div className={clsx(styles.container, {
+      [styles.alignCenter]: align === 'center',
+      [styles.alignRight]: align === 'right',
+    })}>
+      <img src={src} alt={alt ?? ''} width={width ?? '100%'}/>
+    </div>
   );
 };
 
