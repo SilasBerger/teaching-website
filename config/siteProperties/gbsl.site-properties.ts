5a9d804730eba7fb69ce921ad87fc88d7a97f23c
Silas Berger <silas.berger@outlook.com>
2024-05-15 08:53:19 +0200

    Add login button.

--- config/siteProperties/gbsl.site-properties.ts
+++ config/siteProperties/gbsl.site-properties.ts
@@ -1,6 +1,12 @@
 import {SiteProperties} from "../../src/framework/builder/models/site-config";
 
-const navbarItems = [];
+const navbarItems = [
+  {
+    href: '/login',
+    label: '🔑',
+    position: 'right',
+  }
+];
 
 const footer = {
   style: 'dark',
