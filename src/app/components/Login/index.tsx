b043d1011ddb585f6bc725b4186ce1a1d4fe9f70
Silas Berger <silas.berger@outlook.com>
2024-05-16 15:35:21 +0200

    Work on styling login page.

--- src/app/components/Login/index.tsx
+++ src/app/components/Login/index.tsx
@@ -3,8 +3,7 @@ import {LoginStrategy} from "@site/src/app/components/Login/model";
 import LoginLanding from "@site/src/app/components/Login/LoginLanding";
 import {useState} from "react";
 import EmailLogin from "@site/src/app/components/Login/EmailLogin";
-
-const placeholder = <div></div>;
+import clsx from "clsx";
 
 export interface Props {
   loginStrategies: LoginStrategy[];
@@ -16,10 +15,18 @@ const Login = ({loginStrategies}: Props) => {
     loginStrategies={loginStrategies}
     onLoginViaEmail={() => setStep(loginViaEmail)} />
   const [step, setStep] = useState<React.JSX.Element>(loginLanding);
+  const [showBackButton, setShowBackButton] = useState(false);
 
   return (
-    <div>
-      {step}
+    <div className={styles.loginHero}>
+      <div className={styles.header}>
+        {showBackButton && <button className={styles.backButton} onClick={() => setStep(loginLanding)}>
+          <i className={clsx("mdi", "mdi-arrow-left")}></i>
+        </button>}
+      </div>
+      <div className={styles.body}>
+        {step}
+      </div>
     </div>
   );
 };
