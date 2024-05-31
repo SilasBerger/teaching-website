e75ed4baaef3dceb3351a718a15b7947657c3d2b
Silas Berger <silas.berger@outlook.com>
2024-05-31 22:05:03 +0200

    Implement socket.io connection in admin frontend.

--- src/app/components/cipherlock/CipherlockAdmin/index.tsx
+++ src/app/components/cipherlock/CipherlockAdmin/index.tsx
@@ -2,27 +2,108 @@ import styles from "./styles.module.scss";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
 import Admonition from "@site/src/theme/Admonition";
+import io, {Socket} from "socket.io-client";
+import {useState} from "react";
+
+let socket: Socket;
 
 const CipherlockAdmin = () => {
+
+  const [serverConnected, setServerConnected] = useState<boolean>(false);
+  const [connecting, setConnecting] = useState<boolean>(false);
+  const [serverUrl, setServerUrl] = useState<string>('');
+  const [apiKey, setApiKey] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  function resetError() {
+    setError('');
+  }
+
+  function connectToSocket() {
+    setConnecting(true);
+    if (!serverUrl) {
+      setConnecting(false);
+      return;
+    }
+
+    try {
+      const url = new URL(serverUrl);
+      const wsUrl = `ws://${url.host}`;
+      socket = io(wsUrl);
+    } catch (e) {
+      setError(e.toString());
+      setConnecting(false);
+      return;
+    }
+
+    socket.on('connect', () => {
+      setServerConnected(true);
+      setConnecting(false);
+    })
+
+    socket.on('disconnect', () => setServerConnected(false));
+
+    socket.on('connect_error', (error: any) => {
+      setError(error.toString());
+      setConnecting(false);
+    });
+  }
+
+  function disconnectFromServer() {
+    socket.disconnect();
+  }
+
   return (
     <div>
       <div className={styles.connectionPanel}>
 
-        <Admonition type='warning' title='Not connected'>
-          Not connected to game server.
-        </Admonition>
+        {!serverConnected &&
+          <Admonition type='warning' title='Not connected'>
+            Not connected to game server.
+          </Admonition>
+        }
+
+        {error &&
+          <Admonition type='danger' title='Error'>
+            {error}
+          </Admonition>
+        }
 
         <div className={styles.connectionInputs}>
           <div className={styles.connectionInputGroup}>
             <label htmlFor="input-server-url">Game Server URL</label>
-            <input type="text" id="input-server-url" placeholder="https://..." />
+            <input
+              type="text"
+              id="input-server-url"
+              placeholder="https://..."
+              value={serverUrl}
+              onChange={e => setServerUrl(e.target.value)}
+              disabled={connecting || serverConnected}
+            />
           </div>
           <div className={styles.connectionInputGroup}>
             <label htmlFor="input-server-api-key">API Key</label>
-            <input type="text" id="input-server-api-key" placeholder="API key" />
+            <input
+              type="password"
+              id="input-server-api-key"
+              placeholder="API key"
+              value={apiKey}
+              onChange={e => setApiKey(e.target.value)}
+              disabled={connecting || serverConnected}
+            />
           </div>
           <div>
-            <button className="button button--primary">Connect | Disconnect</button>
+            {!serverConnected &&
+              <button
+                className="button button--primary"
+                onClick={() => connectToSocket()}
+                disabled={!(serverUrl && apiKey) || connecting}>Connect</button>
+            }
+            {serverConnected &&
+              <button
+                className="button button--secondary"
+                onClick={() => disconnectFromServer()}>Disconnect</button>
+            }
           </div>
         </div>
       </div>
