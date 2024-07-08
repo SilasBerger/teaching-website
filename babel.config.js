84a98f7c6b3d62e6c7cb5635d7b84d43cde1c182
Silas Berger <silas.berger@outlook.com>
2024-07-08 19:16:43 +0200

    Add support for decorators syntax (EC39 stage 3).

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
   plugins: [
     'transform-decorators-legacy',
-    'transform-class-properties'
+    'transform-class-properties',
+    [require('@babel/plugin-proposal-decorators').default, { version: '2023-05' }]
   ]
 };
