407c6ccf1c16df607e147d958a9c3ca3524d6656
Silas Berger <silas.berger@outlook.com>
2024-08-24 14:12:51 +0200

    Add align property.

--- src/app/components/Struktogramm/index.tsx
+++ src/app/components/Struktogramm/index.tsx
@@ -1,12 +1,13 @@
 import styles from './styles.module.scss';
 
 interface Props {
+  align?: 'flex-start' | 'center' | 'flex-end';
   children: React.ReactNode | React.ReactNode[];
 }
 
-const Struktogramm = ({children}: Props) => {
+const Struktogramm = ({align, children}: Props) => {
   return (
-    <div className={styles.Struktogramm}>
+    <div className={styles.Struktogramm} style={{alignItems: align || 'flex-start'}}>
       <div className={styles.container}>{children}</div>
     </div>
   );
