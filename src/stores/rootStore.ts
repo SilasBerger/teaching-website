1f2e54e992784c2b351074cc2eeb8f03e6216061
Silas Berger <silas.berger@outlook.com>
2024-09-13 09:41:29 +0200

    Add missing rootStore updates.

--- src/stores/rootStore.ts
+++ src/stores/rootStore.ts
@@ -1,75 +1,66 @@
 import React from 'react';
-import {DocumentRootStore} from './DocumentRootStore';
-import {UserStore} from './UserStore';
-import {SessionStore} from './SessionStore';
-import {SocketDataStore} from './SocketDataStore';
-import {action, reaction} from 'mobx';
-import {StudentGroupStore} from './StudentGroupStore';
-import PermissionStore from './PermissionStore';
-import DocumentStore from './DocumentStore';
-import {ToolsStore} from "@site/src/stores/ToolsStore";
-import {PageStore} from "@site/src/stores/PageStore";
+import { DocumentRootStore } from '@tdev-stores/DocumentRootStore';
+import { UserStore } from '@tdev-stores/UserStore';
+import { SessionStore } from '@tdev-stores/SessionStore';
+import { SocketDataStore } from '@tdev-stores/SocketDataStore';
+import { action } from 'mobx';
+import { StudentGroupStore } from '@tdev-stores/StudentGroupStore';
+import PermissionStore from '@tdev-stores/PermissionStore';
+import DocumentStore from '@tdev-stores/DocumentStore';
+import { PageStore } from '@tdev-stores/PageStore';
+import {ToolsStore} from "./ToolsStore";
 
 export class RootStore {
-  documentRootStore: DocumentRootStore;
-  userStore: UserStore;
-  sessionStore: SessionStore;
-  socketStore: SocketDataStore;
-  studentGroupStore: StudentGroupStore;
-  permissionStore: PermissionStore;
-  documentStore: DocumentStore;
-  pageStore: PageStore;
-  toolsStore: ToolsStore;
+    documentRootStore: DocumentRootStore;
+    userStore: UserStore;
+    sessionStore: SessionStore;
+    socketStore: SocketDataStore;
+    studentGroupStore: StudentGroupStore;
+    permissionStore: PermissionStore;
+    documentStore: DocumentStore;
+    pageStore: PageStore;
+    toolsStore: ToolsStore;
 
-  constructor() {
-    this.documentRootStore = new DocumentRootStore(this);
-    this.userStore = new UserStore(this);
-    this.sessionStore = new SessionStore(this);
-    this.socketStore = new SocketDataStore(this);
-    this.studentGroupStore = new StudentGroupStore(this);
-    this.permissionStore = new PermissionStore(this);
-    this.documentStore = new DocumentStore(this);
-    this.pageStore = new PageStore(this);
-    this.toolsStore = new ToolsStore(this);
+    // @observable accessor initialized = false;
+    constructor() {
+        this.documentRootStore = new DocumentRootStore(this);
+        this.sessionStore = new SessionStore(this);
+        this.userStore = new UserStore(this);
+        this.socketStore = new SocketDataStore(this);
+        this.studentGroupStore = new StudentGroupStore(this);
+        this.permissionStore = new PermissionStore(this);
+        this.documentStore = new DocumentStore(this);
+        this.pageStore = new PageStore(this);
+        this.toolsStore = new ToolsStore(this);
 
-    reaction(
-      () => this.sessionStore.isLoggedIn,
-      (isLoggedIn) => {
-        if (isLoggedIn) {
-          this.userStore.loadCurrent().then((user) => {
-            if (user) {
-              this.socketStore.reconnect();
-            }
-          });
-        } else {
-          this.cleanup();
+        if (this.sessionStore.isLoggedIn) {
+            this.load();
         }
-      }
-    );
-  }
+    }
 
-  @action
-  load() {
-    this.userStore.loadCurrent().then((user) => {
-      if (user) {
-        this.socketStore.reconnect();
+    @action
+    load() {
+        this.userStore.loadCurrent().then((user) => {
+            if (user) {
+                this.socketStore.reconnect();
+                /**
+                 * load stores
+                 */
+                this.userStore.load();
+                this.studentGroupStore.load();
+            }
+        });
+    }
+
+    @action
+    cleanup() {
         /**
-         * load stores
+         * could be probably ignored since the page gets reloaded on logout?
          */
-        this.studentGroupStore.load();
-      }
-    });
-  }
-
-  @action
-  cleanup() {
-    /**
-     * could be probably ignored since the page gets reloaded on logout?
-     */
-    this.userStore.cleanup();
-    this.socketStore.cleanup();
-    this.studentGroupStore.cleanup();
-  }
+        this.userStore.cleanup();
+        this.socketStore.cleanup();
+        this.studentGroupStore.cleanup();
+    }
 }
 
 export const rootStore = Object.freeze(new RootStore());
