048d38e56ca715106d72051459be2d61e1ac1f91
Silas Berger <silas.berger@outlook.com>
2024-09-21 11:59:00 +0200

    Move all builder logic into new ScriptsBuilder class.

--- framework/builder/scriptsBuilder.ts
+++ framework/builder/scriptsBuilder.ts
@@ -8,41 +8,52 @@ import {copyFilesToScriptDir, removeObsoleteScriptFiles} from "./sync/file-ops";
 import {applyMarkers, applySectionMappings, collectSyncPairs} from "./sync/sync-tree-processing";
 import {parse} from "yaml";
 import {MATERIAL_ROOT, SCRIPTS_ROOT} from "../../config/builderConfig";
+import {SiteConfig} from "./models/siteConfig";
 
-export function buildScripts(scriptsConfigsFile: string) {
-  const scriptsConfigs = _loadScriptsConfigs(scriptsConfigsFile);
-
-  Object.entries(scriptsConfigs).forEach(([scriptRoot, scriptConfig]: [string, ScriptConfig]) => {
-    _buildScript(scriptRoot, scriptConfig);
-  });
-  return Object.keys(scriptsConfigs);
-}
-
-function _loadScriptsConfigs(scriptsConfigsName: string) {
-  const scriptsConfigsPath = `config/scriptsConfigs/${scriptsConfigsName}`;
-  if (!fs.existsSync(scriptsConfigsPath)) {
-    throw `No such scriptsConfigs file: ${scriptsConfigsPath}`;
+export class ScriptsBuilder {
+  constructor(private _siteConfig: SiteConfig, private _scriptConfigs: { [key: string]: ScriptConfig }) {
   }
-  return parse(fs.readFileSync(scriptsConfigsPath).toString());
-}
 
-function _createMaterialTree(): SourceNode {
-  const materialRootPath = osPath.resolve(MATERIAL_ROOT);
-  return createSourceTree(materialRootPath);
-}
+  static buildOnce(siteConfig: SiteConfig) {
+    const scriptConfigs = ScriptsBuilder._loadScriptConfigs(siteConfig.properties.scriptsConfigsFile);
+    const builder = new ScriptsBuilder(siteConfig, scriptConfigs);
+    return builder._build();
+  }
 
-function _buildScript(scriptRoot: string, scriptConfig: ScriptConfig) {
-  Log.instance.info(`📝 Building script '${scriptRoot}'`);
-  const materialTree = _createMaterialTree();
-  const scriptRootPath = osPath.resolve(osPath.join(SCRIPTS_ROOT, scriptRoot));
-  const scriptTree = createDestTree(scriptRootPath);
-  _synchronizeToScript(materialTree, scriptTree, scriptConfig);
-}
+  private _build() {
+    Log.instance.info(`🔧 Building site '${this._siteConfig.siteId}'`);
+    Object.entries(this._scriptConfigs).forEach(([scriptRoot, scriptConfig]: [string, ScriptConfig]) => {
+      this._buildScript(scriptRoot, scriptConfig);
+    });
+    return Object.keys(this._scriptConfigs);
+  }
 
-function _synchronizeToScript(materialTree: SourceNode, scriptTree: DestNode, scriptConfig: ScriptConfig): void {
-  applySectionMappings(scriptConfig, scriptTree, materialTree);
-  applyMarkers(materialTree, scriptTree, scriptConfig.markers);
-  const syncPairs = collectSyncPairs(scriptTree);
-  copyFilesToScriptDir(syncPairs);
-  removeObsoleteScriptFiles(scriptTree);
+  private static _loadScriptConfigs(scriptsConfigsName: string) {
+    const scriptsConfigsPath = `config/scriptsConfigs/${scriptsConfigsName}`;
+    if (!fs.existsSync(scriptsConfigsPath)) {
+      throw `No such scriptsConfigs file: ${scriptsConfigsPath}`;
+    }
+    return parse(fs.readFileSync(scriptsConfigsPath).toString());
+  }
+
+  private _createMaterialTree(): SourceNode {
+    const materialRootPath = osPath.resolve(MATERIAL_ROOT);
+    return createSourceTree(materialRootPath);
+  }
+
+  private _buildScript(scriptRoot: string, scriptConfig: ScriptConfig) {
+    Log.instance.info(`📝 Building script '${scriptRoot}'`);
+    const materialTree = this._createMaterialTree();
+    const scriptRootPath = osPath.resolve(osPath.join(SCRIPTS_ROOT, scriptRoot));
+    const scriptTree = createDestTree(scriptRootPath);
+    this._synchronizeToScript(materialTree, scriptTree, scriptConfig);
+  }
+
+  private _synchronizeToScript(materialTree: SourceNode, scriptTree: DestNode, scriptConfig: ScriptConfig): void {
+    applySectionMappings(scriptConfig, scriptTree, materialTree);
+    applyMarkers(materialTree, scriptTree, scriptConfig.markers);
+    const syncPairs = collectSyncPairs(scriptTree);
+    copyFilesToScriptDir(syncPairs);
+    removeObsoleteScriptFiles(scriptTree);
+  }
 }
