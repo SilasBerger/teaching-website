3b921ce977a2b3c79477622ea1fab86f6f40d8be
Silas Berger <silas.berger@outlook.com>
2024-10-07 07:49:08 +0200

    Overhaul admonition defs.

--- src/theme/Admonition/icons.tsx
+++ src/theme/Admonition/icons.tsx
@@ -18,6 +18,15 @@ export function WarningIcon(): React.JSX.Element {
   );
 }
 
+// https://icon-sets.iconify.design/mdi/check-circle/
+export function SuccessIcon(): React.JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>check-circle</title>
+      <path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z"/>
+    </svg>
+  );
+}
+
 // https://icon-sets.iconify.design/mdi/crown-outline/
 export function KeyIcon() {
   return (
