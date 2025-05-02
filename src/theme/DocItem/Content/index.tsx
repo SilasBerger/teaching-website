8a362140fcc74afc6573ecbb2a7f8f3e803c0f11
Silas Berger <silas.berger@outlook.com>
2025-05-02 10:53:26 +0200

    Include remaining update parts.

--- src/theme/DocItem/Content/index.tsx
+++ src/theme/DocItem/Content/index.tsx
@@ -2,25 +2,14 @@ import React from 'react';
 import Content from '@theme-original/DocItem/Content';
 import type ContentType from '@theme/DocItem/Content';
 import type { WrapperProps } from '@docusaurus/types';
-import { useDoc } from '@docusaurus/plugin-content-docs/client';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@tdev-hooks/useStore';
 import { useLocation } from '@docusaurus/router';
 type Props = WrapperProps<typeof ContentType>;
 
-const ContentWrapper = observer((props: Props): JSX.Element => {
-    const { frontMatter } = useDoc();
+const ContentWrapper = observer((props: Props): React.ReactNode => {
     const pageStore = useStore('pageStore');
-    const pageId = (frontMatter as { page_id: string }).page_id;
     const location = useLocation();
-    React.useEffect(() => {
-        if (pageId) {
-            pageStore.addIfNotPresent(pageId, true);
-        }
-        return () => {
-            pageStore.setCurrentPageId(undefined);
-        };
-    }, [pageId]);
 
     React.useEffect(() => {
         if (pageStore.current) {
