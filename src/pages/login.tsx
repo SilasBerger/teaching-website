150c9ee8b24baabdb3e514d58b3dc3d02cb5f5d8
Silas Berger <silas.berger@outlook.com>
2025-05-08 09:14:23 +0200

    Remove search on certain pages.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,15 @@
 import LoginPage from "@site/src/components/LoginPage";
+import Head from '@docusaurus/Head';
 
-export default LoginPage;
+const LoginPageWrapper = () => {
+    return (
+        <>
+            <Head>
+                <body className='no-search'></body>
+            </Head>
+            <LoginPage />
+        </>
+    );
+};
+
+export default LoginPageWrapper;
